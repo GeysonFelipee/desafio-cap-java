72ff315497ff752924fa338d0e1218d9d01b6575
GeysonFelipee <geysonfelipee@gmail.com>
2022-02-20 20:14:38 -0300

    criadp todas as tarefas

--- src/main/java/br/com/geyson/desafio/principal/Run.java
+++ src/main/java/br/com/geyson/desafio/principal/Run.java
@@ -1,7 +1,13 @@
 package br.com.geyson.desafio.principal;
 
+import br.com.geyson.desafio.principal.ratrefas.Anagrama;
+import br.com.geyson.desafio.principal.ratrefas.Escada;
+import br.com.geyson.desafio.principal.ratrefas.Senha;
+
 public class Run {
     public static void main(String[] args) {
-
+        new Anagrama().find("ifailuhkqq");
+        new Senha().validar("Ya3");
+        new Escada().build(6);
     }
 }
