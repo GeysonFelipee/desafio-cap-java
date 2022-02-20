dfbe416bfe12d0694bfec75b968c2e2bf98ebb58
GeysonFelipee <geysonfelipee@gmail.com>
2022-02-20 20:07:06 -0300

    criado projeto, modificação do arquivo pom, creado a clas main e a class de test

---
+++ src/main/java/br/com/geyson/desafio/principal/Run.java
@@ -0,0 +1,7 @@
+package br.com.geyson.desafio.principal;
+
+public class Run {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/br/com/geyson/desafio/test/TesteDesafioCap.java
@@ -0,0 +1,7 @@
+package br.com.geyson.desafio.test;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class TesteDesafioCap {
+}
